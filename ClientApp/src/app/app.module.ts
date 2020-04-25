7bd1e6651da0d618ee987e4516c39a346170e5ae
Orin Walker <orin.walker@gmail.com>
2020-04-25 15:00:47 -0400

    Added hacker-news component and modified routes and menus to set as home page

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HackerNewsComponent } from './hacker-news/hacker-news.component';
+
 
 @NgModule({
   declarations: [
@@ -16,16 +18,23 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    HackerNewsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      //{ path: '', component: HomeComponent, pathMatch: 'full' },
+      // TODO: cleanup comments
+      // TODO: remove the other routes that are not being used
+      // TODO: Add 404 and error page handling
+      { path: '', component: FetchDataComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'home', component: HomeComponent }
+      //{ path: 'hacker-news', component: FetchDataComponent },
     ])
   ],
   providers: [],
