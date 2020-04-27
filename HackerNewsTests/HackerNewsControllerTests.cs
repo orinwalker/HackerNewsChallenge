1770e2bf921783fe5cc6f6f3b0eae52e8e32aeab
Orin Walker <orin.walker@gmail.com>
2020-04-27 10:39:58 -0400

    Finished the controller test

--- HackerNewsTests/HackerNewsControllerTests.cs
+++ HackerNewsTests/HackerNewsControllerTests.cs
@@ -1,111 +1,61 @@
-﻿using AutoMapper;
-using HackerNewsModernUI;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using HackerNewsModernUI.Controllers;
 using HackerNewsModernUI.Models;
 using HackerNewsModernUI.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace HackerNewsTests
 {
-    //public class HackerNewsServiceTest
-    //{
-        //private readonly HackerNewsService _service;
-        //private readonly IMapper _mapper;
-
-       
-
-       
-
-
-    //}
-
-
     public class HackerNewsControllerTests
     {
-        // TODO: Pull in the ILogger 
-        private readonly ILogger _logger;
+        [Fact]
+        public void TestController()
+        {
+            // Arrange 
+            var mockRepo = new Mock<IHackerNewsCachedService>();
+            mockRepo.Setup(repo => repo.GetAllTopStoriesAsync())
+                .Returns(GetMockHackerNewsArticles());
+            var logger = GetLogger();
+            var controller = new HackerNewsController(logger, mockRepo.Object);
 
-        // TODO: Create this using MOQ
-        //private readonly _hackerNewsService = null;
+            // Act
+            var rv = controller.Get();
+            var result = rv.Result.ToArray(); // TODO: combine with line above
 
-        // TODO: this needs created
-        //private readonly _contextMock = null;
+            // Assert
+            var type = Assert.IsType<Task<IEnumerable<IHackerNewsArticle>>>(rv); // TODO: change this to result
+            Assert.Equal(2, result.Count());
+            Assert.Equal(8863, result[0].Id);
+            Assert.Equal("My YC app: Dropbox - Throw away your USB drive", result[0].Title);
+            Assert.Equal(8864, result[1].Id);
+            Assert.Equal("https://rust-analyzer.github.io/blog/2020/04/20/first-release.html", result[1].Url);
+        }
 
-        private static ILogger<HackerNewsController> GetLogger()
+        private ILogger<HackerNewsController> GetLogger()
         {
             var serviceProvider = new ServiceCollection()
                 .AddLogging()
                 .BuildServiceProvider();
 
             var factory = serviceProvider.GetService<ILoggerFactory>();
-
-            // TODO: I don't understand this code
             var logger = factory.CreateLogger<HackerNewsController>();
             return logger;
         }
-      
 
-        [Fact]
-        public void TestLogger()
-        {
-            _logger.LogInformation("xyz");
-            Assert.True(true);
-        }
-
-        [Fact]
-        public void TestController()
-        {
-            // Arrange 
-            //var articleId = 8863;
-            //var mockRepo = new Mock<IHackerNewsService>();
-            //mockRepo.Setup(repo => repo.GetNewsAsync(articleId))
-            //    .Returns(GetMockHackerNewsArticle);
-            //var hackerNewsService = new HackerNewsService();
-            //var result = await hackerNewsService.GetNewsAsync(articleId);
-
-            var mockRepo = new Mock<IHackerNewsCachedService>();
-            mockRepo.Setup(repo => repo.GetAllTopStoriesAsync())
-                .Returns(GetMockHackerNewsArticles());
-            var logger = GetLogger();
-            //string logger = "";
-            var controller = new HackerNewsController(logger, mockRepo.Object);
-            var result = controller.Get();
-
-            // Assert
-            var type = Assert.IsType<Task<IEnumerable<IHackerNewsArticle>>>(result);
-
-
-            //var count = result.ToList().Count;
-            //var res = Assert.
-
-            //var viewResult = Assert.IsType<ViewResult>(result);
-            //var model = Assert.IsType<StormSessionViewModel>(
-            //    viewResult.ViewData.Model);
-            //Assert.Equal("Test One", model.Name);
-            //Assert.Equal(2, model.DateCreated.Day);
-            //Assert.Equal(testSessionId, model.Id);
-        }
-
-        // Task<IEnumerable<IHackerNewsArticle>>
         private Task<IEnumerable<IHackerNewsArticle>> GetMockHackerNewsArticles()
         {
             var hackerArticles = new List<IHackerNewsArticle>()
               {
                new HackerNewsArticle
                {
-                   By = "dhouston",
+                     By = "dhouston",
                      Descendants = 71,
                      Id = 8863,
-                    // Kids = [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
                      Score = 111,
                      Time = 1175714200,
                      Title = "My YC app: Dropbox - Throw away your USB drive",
@@ -114,77 +64,18 @@ namespace HackerNewsTests
                 },
                new HackerNewsArticle
                 {
-                     By = "dhouston",
-                     Descendants = 71,
-                     Id = 8863,
-                     // Kids = [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
-                     Score = 111,
-                     Time = 1175714200,
-                     Title = "My YC app: Dropbox - Throw away your USB drive",
+                     By = "groar",
+                     Descendants = 6,
+                     Id = 8864,
+                     Score = 234,
+                     Time = 1154424200,
+                     Title = "YOLOv4: Optimal Speed and Accuracy of Object Detection",
                      Type = "story",
-                     Url = "http://www.getdropbox.com/u/2/screencast.html"
+                     Url = "https://rust-analyzer.github.io/blog/2020/04/20/first-release.html"
                   }
                };
 
             return Task.FromResult(hackerArticles.AsEnumerable());
         }
-
-        //private Task<HackerNewsArticle> GetMockHackerNewsArticles()
-        //{
-        //    var hackerNewsArticle = new HackerNewsArticle
-        //    {
-        //        By = "ORIN: dhouston",
-        //        Descendants = 71,
-        //        Id = 8863,
-        //        // Kids = [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
-        //        Score = 111,
-        //        Time = 1175714200,
-        //        Title = "ORIN: My YC app: Dropbox - Throw away your USB drive",
-        //        Type = "story",
-        //        Url = "http://www.getdropbox.com/u/2/screencast.html"
-        //    };
-        //    return Task.FromResult(hackerNewsArticle);
-        //}
-
-        ////Arrange
-        //var mockLeagueService = new MockLeagueService().MockGetAll(new List<League>());
-
-        //var controller = new LeagueController(mockLeagueService.Object);
-
-        ////Act
-        //var result = controller.Index();
-
-        ////Assert
-        //Assert.IsAssignableFrom<ViewResult>(result);
-        // mockLeagueService.VerifyGetAll(Times.Once());
-
-
-
-        //[Fact]
-        //public async Task Get_order_detail_success()
-        //{
-
-        //Arrange
-        //var fakeOrderId = "12";
-        //var fakeOrder = GetFakeOrder();
-
-        //...
-        //var mockHackerNewsService = new MockHackerNewsService().MockGet();
-        //Act
-        //var hackerNewsController = new HackerNewsController(
-        //    _logger,
-        //    _hackerNewsService.Object);
-
-        //hackerNewsController.ControllerContext.HttpContext = _contextMock.Object;
-        //var result = await hackerNewsController.Get();
-
-        //Assert
-        //var viewResult = Assert.IsType<Task<IEnumerable<IHackerNewsArticle>>>(result);
-
-        // TODO: Figure out how check if result is 'OK' (200 status code)
-        //viewResult.Result
-        //Assert.IsAssignableFrom<Order>(viewResult.ViewData.Model);
-
-        //}
     }
 }
