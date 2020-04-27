a04ffdc5e13ee7c947fbc62239dae535219d2d4b
Orin Walker <orin.walker@gmail.com>
2020-04-27 11:33:33 -0400

    added more unit tests

--- HackerNewsTests/HackerNewsServiceTests.cs
+++ HackerNewsTests/HackerNewsServiceTests.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -14,30 +15,8 @@ namespace HackerNewsTests
 {
     public class HackerNewsServiceTests
     {
-        private readonly ILogger _logger;
-
-        public HackerNewsServiceTests()
-        {
-            var serviceProvider = new ServiceCollection()
-               .AddLogging()
-               .BuildServiceProvider();
-
-            var factory = serviceProvider.GetService<ILoggerFactory>();
-
-            _logger = factory.CreateLogger<HackerNewsController>();
-        }
-
-        
-
         [Fact]
-        public void TestLogger()
-        {
-            _logger.LogInformation("xyz");
-            Assert.True(true);
-        }
-
-        [Fact]
-        public async void HackerNewsServiceTest()
+        public async void HackerNewsService_GetNewsAsync_ById()
         {
             // Arrange 
             var articleId = 8863;
@@ -45,66 +24,48 @@ namespace HackerNewsTests
             mockRepo.Setup(repo => repo.GetNewsAsync(articleId))
                 .Returns(GetMockHackerNewsArticle);
             var hackerNewsService = new HackerNewsService();
-            var result = await hackerNewsService.GetNewsAsync(articleId);
+
             // Act
-
-            mockRepo.Verify();
-
-
-            //    //mockRepo.Setup(repo =>
-            //    //repo.GetAllAsync()).ReturnsAsync(employeeDetail.ToList());
-            //    //Task<HackerNewsArticle> output = null;
-            //    //output = mockRepo.Setup(repo => repo.GetNewsAsync(articleId)); //.ReturnsAsync(output);
-            //mockRepo.Setup(repo => repo.GetNewsAsync(
-            //    It.IsAny<int>()))
-            //    .ReturnsAsync((int i) =>
-            //    hackerArticles.SingleOrDefault(x => x.Id == i));
-
+            var rv = await hackerNewsService.GetNewsAsync(articleId);
+            
             // Assert
-            //var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
-            //Assert.Null(redirectToActionResult.ControllerName);
-            //Assert.Equal("Index", redirectToActionResult.ActionName);
-
-
-
-            // mockRepo.SetupAllProperties();
-            //.ReturnsAsync(GetMockHackerNewsArticle());
-
-            //     mockRepo.Setup(repo => repo.ListAsync())
-            //.ReturnsAsync(GetTestSessions());
-
-            //    mockRepo.Setup(repo => repo.GetEmployeeDetailById(
-            //       It.IsAny<int>())).ReturnsAsync((int i) =>
-            //       employeeDetail.SingleOrDefault(x => x.EId == i));
-
-            //    mockRepo.Setup(repo => repo.Insert(It.IsAny<EmployeeDetail>()))
-            //    .Callback((EmployeeDetail employeeDetails) =>
-            //    {
-            //        employeeDetails.EId = employeeDetail.Count() + 1;
-            //        employeeDetail.Add(employeeDetails);
-            //    }).Verifiable();
-
-            //    mockRepo.Setup(repo => repo.Delete(It.IsAny<EmployeeDetail>()))
-            //    .Callback((EmployeeDetail employeeDetails) =>
-            //    {
-            //        employeeDetails.EId = employeeDetail.Count() - 1;
-            //        employeeDetail.Remove(employeeDetails);
-            //    }).Verifiable();
-
-
-            //_mapper = WebAPI.Mapper.Mapper.GetMapper();
-
-            //_service = new HackerNewsService(mockRepo.Object);
+            Assert.Equal(8863, rv.Id);
+            Assert.Equal("dhouston", rv.By);
         }
 
+        public async void HackerNewsService_GetTopArticleIds()
+        {
+            // Arrange 
+            var mockRepo = new Mock<IHackerNewsService>();
+            mockRepo.Setup(repo => repo.GetTopStoriesAsync())
+                .Returns(GetMockTopArticleIds);
+            var hackerNewsService = new HackerNewsService();
+
+            // Act
+            var rv = await hackerNewsService.GetTopStoriesAsync();
+            var result = rv.ToArray();
+            // Assert
+            Assert.Equal(7463, result[0]);
+            Assert.Equal(7464, result[1]);
+            Assert.Equal(7465, result[2]);
+        }
+
+        private Task<IEnumerable<int>> GetMockTopArticleIds()
+        {
+            var list = new List<int>();
+            list.Add(7463);
+            list.Add(7464);
+            list.Add(7465);
+            return Task.FromResult(list.AsEnumerable());
+        }
+        
         private Task<HackerNewsArticle> GetMockHackerNewsArticle()
         {
             var hackerNewsArticle = new HackerNewsArticle
             {
-                By = "ORIN: dhouston",
+                By = "dhouston",
                 Descendants = 71,
                 Id = 8863,
-                // Kids = [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ],
                 Score = 111,
                 Time = 1175714200,
                 Title = "ORIN: My YC app: Dropbox - Throw away your USB drive",
